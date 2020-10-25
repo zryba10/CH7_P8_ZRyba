d575060045c53865854fb470f2c2a0b4d881b2c8
zryba10 <73262854+zryba10@users.noreply.github.com>
2020-10-25 17:36:52 -0500

    Update CH7_P8_ZRyba.py

--- CH7_P8_ZRyba.py
+++ CH7_P8_ZRyba.py
@@ -16,26 +16,26 @@ def grayscale(image):
             b = int(b * 0.114)
             lum = r + g + b
             image.setPixel(x, y, (lum, lum, lum))
-            sepia(image)
 def sepia(image):
+    grayscale(image)
     for y in range(image.getHeight()):
         for x in range(image.getWidth()):
             (r, g, b) = image.getPixel(x, y)
-            if r < 63:
+            if r<63:
                 r = int(r * 1.1)
                 b = int(b * 0.9)
-            elif red < 192:
+            elif r<192:
                 r = int(r * 1.15)
                 b = int(b * 0.85)            
             else:
-                r = min(int(r * 1.08), 255)
+                r = min(int(r * 1.08),255)
                 b = int(b * 0.93)
             image.setPixel(x, y, (r, g, b))
 def main(filename = "smokey.gif"):
     image = Image(filename)
     print("Close the image window to continue. ")
     image.draw()
-    grayscale(image)
+    sepia(image)
     print("Close the image window to quit. ")
     image.draw()
 
